7fccc03f107d057199a58602af21724c1d2e44c9
Fredrik Eilertsen <fredrik.eilertsen@gmail.com>
2016-09-12 19:44:58 +0200

    Bug fixes

--- iOS-Sensor/Gyroscope.m
+++ iOS-Sensor/Gyroscope.m
@@ -9,8 +9,9 @@
 
 
 % Application properties
-port       = 31415;      % Standard port(31415)
-password   = 'password';   % Password for iOS app
+port       = 31415;       % Standard port(31415)
+password   = 'password';  % Password for iOS app
+numOfLoops = 150;         % Number of datapoints 
 
 
 % Sensor properties:
@@ -36,27 +37,34 @@ m.OrientationSensorEnabled      = orientationSensor;
 m.PositionSensorEnabled         = positionSensor;
 m.SampleRate                    = sampleRate;
 
+
+% Start logging
 m.Logging = 1;
 
 
 % Run a loop, checking for orientation
-for k = 1:250
+for k = 1:numOfLoops
     
-    % Acquire 1 second of data
-    pause(0.3)
-    [o,t] = orientlog(m);
+    % Pause to aquire data
+    pause(0.2)
+    
+    % Read orientationSensor log into o, t.
+    [o, t] = orientlog(m);
+    
+    % Discard orientationSensor log
     discardlogs(m)
     
-    %~ not operator
     
-    if ~isempty(o)   
+    if ~isempty(o)         %~ not operator 
         
-        % Check Orientation
-        z = mean(o(:,3));
+        % Compute mean value of the 3 column, roll/z axis.
+        z = mean(o(:,3));  % in degrees 
         
         % Check Z-axis to determine which way phone is facing
-        if (z > 75 && z < 95) || (z < -75 && z > - 95)
-            disp('Side')
+        if (z > 75 && z < 95)
+            disp('R Side')
+        elseif (z < -75 && z > - 95)
+            disp('L Side')
         elseif z > -100 && z < 100
             disp('Up')
         else
