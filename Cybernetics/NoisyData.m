581f92eeb7e9c3c89384e48bd683570bc5ac3624
fredeil <fredeil@gmail.com>
2017-04-10 08:54:31 +0200

    fixed

--- Cybernetics/NoisyData.m
+++ Cybernetics/NoisyData.m
@@ -1,9 +1,13 @@
-%% Simulate some noisy data, can be used for applying filters etc.
-
+%% Simulate some noisy data
+% can be used for applying filters etc.
 x = 1:.1:10;
 T = 0.1*x + 1;
 Tn = T + 0.1*randn(size(x));
 plot(x, Tn, '.');
-hold on;
-plot(x, medfilt1(Tn), '*');
-legend('Unfiltered', 'Filtered', 'location', 'best')
+
+%% Simulate drag coefficient d = kv^2
+% this data replicates what you migth expect in a wind tunnel.
+v = 0:1:60;
+d = 0.1234*v.^2;
+dn = d + 0.4*v.*randn(size(v));
+plot(v, dn, '*');
