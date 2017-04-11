b86c55bc4808810f027b861c7235f57451382c6f
fredeil <fredeil@gmail.com>
2017-04-11 08:00:51 +0200

    P controller

--- Cybernetics/SimplePID.m
+++ Cybernetics/SimplePID.m
@@ -1,16 +1,9 @@
 clear, close, clc
 
-Kp = 1;
+Kp = 2;
 Hs = tf(1.8, [0 15 1], 'ioDelay', 6);
 
-
-for i = [.1 .2 .3 .5]
-hold on
-P = pid(i);
-Hr = feedback(P*Hs, 1);
-step(Hr);
-end
-
-
-
+Hr = pid(Kp)
+T = feedback(P*Hs, 1)
+step(T);
 
